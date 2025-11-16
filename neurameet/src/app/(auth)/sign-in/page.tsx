2038f82e7ffa3250183992064563c85e1c439cd8
Dev Srivastava <devsrivastav64@gmail.com>
2025-11-16 23:00:26 +0530

    updated codes

---
+++ neurameet/src/app/(auth)/sign-in/page.tsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import {Card} from "@/components/ui/card"
+
+const page = () => {
+  return (
+    <div>
+      <Card>Sign In</Card> 
+    </div>
+  )
+}
+
+export default page
---
+++ neurameet/src/app/(auth)/sign-up/page.tsx
@@ -0,0 +1,12 @@
+import { Card } from '@/components/ui/card'
+import React from 'react'
+
+const page = () => {
+  return (
+    <div>
+     <Card>Sign Up Page</Card> 
+    </div>
+  )
+}
+
+export default page
