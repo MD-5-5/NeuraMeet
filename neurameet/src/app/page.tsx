04b7b99006b61598a34a10224bf1a53912adfb9e
Dev Srivastava <devsrivastav64@gmail.com>
2025-11-16 22:31:20 +0530

    auth completed

--- neurameet/src/app/page.tsx
+++ neurameet/src/app/page.tsx
@@ -7,14 +7,15 @@ import { authClient } from "@/lib/auth-client"
 import { useState } from 'react';
 
 export default function Home () {
+
+  const { data: session } = authClient.useSession() 
   const [email, setEmail] = useState("")
   const [name, setName] = useState("")
   const [password, setPassword] = useState("")
 
-  const Onsubmit =()=>{
-    authClient.signUp.email({
+  const OnLogin =()=>{
+    authClient.signIn.email({
       email, // user email address
-      name,
       password, // user password -> min 8 characters by default
     },{
       onError: ()=>{
@@ -25,12 +26,28 @@ export default function Home () {
       }
     })
   }
+
+  if(session){
+    return (
+      <div className='flex flex-col p-4 gap-y-4'>
+        <p>Logged in as {session.user.name} </p>
+        <Button onClick={()=> authClient.signOut()}>Sign Out</Button>
+      </div>
+    )
+  }
   return (
-    <div className='p-4 flex flex-col gap-y-4'>
-      <Input placeholder='name' value={name} onChange={(e)=> setName(e.target.value)}/>
-      <Input placeholder='email' value={email} onChange={(e)=> setEmail(e.target.value)}/>
-      <Input placeholder='password' value={password} onChange={(e)=> setPassword(e.target.value)}/>
-      <Button onClick={Onsubmit} className= 'bg-amber-200'>Create User</Button>
+    <div className='flex flex-col gap-y-10'>
+      <div className='p-4 flex flex-col gap-y-4'>
+        <Input placeholder='name' value={name} onChange={(e)=> setName(e.target.value)}/>
+        <Input placeholder='email' value={email} onChange={(e)=> setEmail(e.target.value)}/>
+        <Input placeholder='password' value={password} onChange={(e)=> setPassword(e.target.value)}/>
+        <Button onClick={OnLogin} className= 'bg-amber-200'>Login</Button>
+      </div>
+      <div className='p-4 flex flex-col gap-y-4'>
+        <Input placeholder='email' value={email} onChange={(e)=> setEmail(e.target.value)}/>
+        <Input placeholder='password' value={password} onChange={(e)=> setPassword(e.target.value)}/>
+        <Button onClick={OnLogin} className= 'bg-amber-200'>Login</Button>
+      </div>
     </div>
   )
 }
