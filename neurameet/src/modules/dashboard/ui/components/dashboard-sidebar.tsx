9a9e048aa8ddf4cf0e3458a9078fad6277901d3c
Dev Srivastava <devsrivastav64@gmail.com>
2025-11-18 22:10:26 +0530

    updated codes

--- neurameet/src/modules/dashboard/ui/components/dashboard-sidebar.tsx
+++ neurameet/src/modules/dashboard/ui/components/dashboard-sidebar.tsx
@@ -17,6 +17,7 @@ import {
 } from "@/components/ui/sidebar"
 import { icons } from "lucide-react"
 import { Separator } from "@/components/ui/separator"
+import { usePathname } from "next/navigation"
 
 
 const firstSection = [
@@ -41,6 +42,7 @@ const secondSection = [
 
 
 export const DashboardSidebar = () => {
+    const pathname = usePathname()
     return (
         <Sidebar>
             <SidebarHeader className="text-sidebar-accent-foreground">
@@ -61,8 +63,11 @@ export const DashboardSidebar = () => {
                                     <SidebarMenuButton 
                                         asChild
                                         className={cn(
-                                        "h-10 hover:bg-linear-to-r/oklch border border-transparent hover:border-[#5D6B68]/10 from-sidebar-accent from-5% via-30% via-sidebar/50 to-sidebar/50"
-                                        )}>
+                                        "h-10 hover:bg-linear-to-r/oklch border border-transparent hover:border-[#5D6B68]/10 from-sidebar-accent from-5% via-30% via-sidebar/50 to-sidebar/50",
+                                        pathname === item.href && "bg-linear-to-r/oklch border-[#5D6B68]/10"
+                                        )}
+                                        isActive={pathname === item.href}
+                                        >
                                         <Link href={item.href}>
                                             <item.icon className="size-3"/>
                                             <span className="text-sm font-medium tracking-tight">
