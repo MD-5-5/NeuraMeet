b34cb87023b8581e5127b40e9e4feb5611f5f30d
Dev Srivastava <devsrivastav64@gmail.com>
2025-11-19 00:06:08 +0530

    done with sidebar

--- neurameet/src/modules/dashboard/ui/components/dashboard-user-button.tsx
+++ neurameet/src/modules/dashboard/ui/components/dashboard-user-button.tsx
@@ -9,10 +9,22 @@ import {
 } from "@/components/ui/dropdown-menu"
 import { Avatar, AvatarImage } from "@/components/ui/avatar";
 import { GeneratedAvatar } from "@/components/generate-avatar";
+import { ChevronDownIcon, CreditCardIcon, LogOutIcon } from "lucide-react";
+import { useRouter } from "next/navigation";
 
 export const DashboardUserButton = () =>{
-
+    const router = useRouter();
     const {data, isPending} = authClient.useSession();
+
+    const onLogout = () =>{
+        authClient.signOut({
+            fetchOptions:{
+                onSuccess:() => {
+                    router.push("/sign-in")
+                }
+            }
+        })
+    }
 
     if(isPending || !data?.user){
         return null;
@@ -31,7 +43,38 @@ export const DashboardUserButton = () =>{
                     className="size-9 mr-3"
                     />
                 )}   
+                <div className="flex flex-col gap-0.5 text-left overflow-hidden flex-1 min-w-0">
+                    <p className="text-sm truncate w-full">
+                        {data.user.name}
+                    </p>
+                    <p className="text-xs truncate w-full">
+                        {data.user.email}
+                    </p>
+                </div>
+                <ChevronDownIcon className="size-4 shrink-0"/>
             </DropdownMenuTrigger>
+            <DropdownMenuContent align="end" side="right" className="w-72">
+                <DropdownMenuLabel>
+                    <div className="flex flex-col gap-1">
+                        <span className="font-medium truncate">{data.user.name}</span>
+                        <span className="text-sm font-normal text-muted-foreground truncate">{data.user.email}</span>
+                    </div>
+                </DropdownMenuLabel>
+                <DropdownMenuSeparator/>
+                <DropdownMenuItem
+                className="cursor-pointer flex items-center justify-between"
+                >
+                    Billing
+                  <CreditCardIcon className="size-4"/>
+                </DropdownMenuItem>
+                <DropdownMenuItem
+                onClick={onLogout}
+                className="cursor-pointer flex items-center justify-between"
+                >
+                    LogOut
+                  <LogOutIcon className="size-4"/>
+                </DropdownMenuItem>
+            </DropdownMenuContent>
         </DropdownMenu>
     )
 }
