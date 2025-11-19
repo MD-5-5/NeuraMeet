4b25ec202a5203f967d369be632c1e538ad2b847
Dev Srivastava <devsrivastav64@gmail.com>
2025-11-19 22:37:04 +0530

    Dashboard searchbar added

--- neurameet/src/modules/dashboard/ui/components/dashboard-user-button.tsx
+++ neurameet/src/modules/dashboard/ui/components/dashboard-user-button.tsx
@@ -31,7 +31,7 @@ export const DashboardUserButton = () =>{
     }
     return(
         <DropdownMenu>
-            <DropdownMenuTrigger className="rounded-lg border-border/10 p-3 w-full flex items-center justify-center bg-white/5 hover:bg-white/10 overflow-hidden">
+            <DropdownMenuTrigger className="rounded-lg border-border/10 p-3 w-full flex items-center justify-center bg-white/5 hover:bg-white/10 overflow-hidden gap-x-2">
                 {data.user.image ? (
                     <Avatar>
                         <AvatarImage src={data.user.image}/>
