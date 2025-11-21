572a0e01b06ee2f133e78bea558ea50d46bbd38a
Dev Srivastava <devsrivastav64@gmail.com>
2025-11-21 14:16:59 +0530

    Agents Setup

---
+++ neurameet/src/modules/agents/server/procedures.ts
@@ -0,0 +1,19 @@
+import { agents } from "@/database/Schema";
+import { createTRPCRouter, baseProcedure } from "@/trpc/init";
+import { db } from "@/database";
+import { resolve } from "path";
+import { TRPCError } from "@trpc/server";
+
+export const agentsRouter = createTRPCRouter({
+    getMany: baseProcedure.query(async()=>{
+        const data = await db
+        .select()
+        .from(agents);
+
+        // await new Promise((resolve) => setTimeout(resolve, 5000))
+
+        throw new TRPCError({code: "BAD_REQUEST"})
+
+        return data
+    })
+})
