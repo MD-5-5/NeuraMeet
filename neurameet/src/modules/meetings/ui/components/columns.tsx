3ea9d0772db9d2e28aaf879796a7682be8b2d2b2
Dev Srivastava <devsrivastav64@gmail.com>
2025-12-16 23:38:36 +0530

    added summary page

--- neurameet/src/modules/meetings/ui/components/columns.tsx
+++ neurameet/src/modules/meetings/ui/components/columns.tsx
@@ -3,19 +3,18 @@
 import { ColumnDef } from "@tanstack/react-table"
 import { MeetingGetmany } from "../../types"
 import { format } from "date-fns"
-import humanizeDuration from "humanize-duration"
 import { GeneratedAvatar } from "@/components/generate-avatar"
 import { CircleCheckIcon, CircleXIcon, ClockArrowUpIcon, ClockFadingIcon, CornerDownRightIcon, LoaderIcon} from "lucide-react"
 import { Badge } from "@/components/ui/badge"
-import { cn } from "@/lib/utils"
+import { cn, formatDuration } from "@/lib/utils"
 
-function formatDuration(seconds: number){
-    return humanizeDuration(seconds * 1000, {
-        largest: 1,
-        round: true,
-        units: ["h", "m","s"],
-    })
-}
+// function formatDuration(seconds: number){
+//     return humanizeDuration(seconds * 1000, {
+//         largest: 1,
+//         round: true,
+//         units: ["h", "m","s"],
+//     })
+// }
 
 const statusIconMap = {
     upcoming: ClockArrowUpIcon,
